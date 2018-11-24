91b5651538e3f075f727439cb46494081d3afeca
wodkab <pluskota@gmx.de>
2018-11-24 20:29:36 +0100

    First steps for directory syncer

---
+++ AACMassEncoder/AACMassEncoder/SyncDirectories.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AACMassEncoder
+{
+
+    public class SyncDirectories
+    {
+        private readonly string SourcePath;
+        private readonly string ToSyncPath;
+        private IList<FileItem> SourceFiles;
+
+        public SyncDirectories(string sourcePath, string toSyncPath, IList<FileItem>sourceFiles)
+        {
+            SourcePath = sourcePath;
+            ToSyncPath = toSyncPath;
+            SourceFiles = sourceFiles;
+        }
+
+        private IEnumerable<string> ToSyncFiles => Directory.GetFiles(ToSyncPath, "*", SearchOption.AllDirectories);
+
+        public void Sync()
+        {
+            var syncFileItems = ToSyncFiles.Select(file =>
+                new FileItem(new FileInfo(file), SourcePath, ToSyncPath, String.Empty)).ToList();
+
+            foreach (var syncFile in syncFileItems)
+            {
+                //No original on source site => delete
+                //if(syncFile)
+            }
+        }
+    }
+}
